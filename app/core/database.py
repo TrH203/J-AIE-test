d1240d29e88f5b083d4d37ce77c181530f2c4f6d
TrH203 <hoangtrhien203@gmail.com>
2025-07-14 14:27:26 +0700

    fix session

--- app/core/database.py
+++ app/core/database.py
@@ -1,12 +1,16 @@
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+from contextlib import asynccontextmanager
 
 DATABASE_URL = os.getenv("DB_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
+
+@asynccontextmanager
 async def get_session() -> AsyncSession:
     async with AsyncSessionLocal() as session:
         yield session
+
